08ab74367d7260aec9c238fd3576bd864f085263
Rachel <rac777blue@hotmail.com>
2020-02-13 11:27:09 +0000

    completed challenge 3

---
+++ app/Stuff/Things/Potato.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Stuff\Things;
+
+class Potato
+{
+    private $watered = 0;
+
+    public function water() : Potato
+    {
+        $this->watered += 1;
+
+        return $this;
+    }
+
+    public function hasGrown() : bool
+    {
+        return $this->watered >= 5;
+    }
+}
+
+// Create a class Potato in the App\Stuff\Things namespace. It should have a water() and hasGrown() method. hasGrown() should return false until the Potato has been watered five or more times.
